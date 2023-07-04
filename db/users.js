2c13d2a5d3c177ba31503f72a8373ea68836ce25
Trusha Nipul Shah <tr335214@dal.ca>
2023-07-04 14:27:51 -0300

    t5

---
+++ db/users.js
@@ -0,0 +1,53 @@
+const users = [
+    {
+        "email": "abc@abc.ca",
+        "firstName": "ABC",
+        "id": "5abf6783"
+    },
+    {
+        "email": "xyz@xyz.ca",
+        "firstName": "XYZ",
+        "id": "5abf674563"
+    },
+    {
+        "email": "john@doe.com",
+        "firstName": "John",
+        "id": "5abf67329c"
+    },
+    {
+        "email": "jane@smith.com",
+        "firstName": "Jane",
+        "id": "5abf67b583"
+    },
+    {
+        "email": "user1@example.com",
+        "firstName": "User1",
+        "id": "5abf67d1a8"
+    },
+    {
+        "email": "user2@example.com",
+        "firstName": "User2",
+        "id": "5abf672b1f"
+    },
+    {
+        "email": "test@test.com",
+        "firstName": "Test",
+        "id": "5abf678e36"
+    },
+    {
+        "email": "example@example.com",
+        "firstName": "Example",
+        "id": "5abf676d42"
+    },
+    {
+        "email": "foo@bar.com",
+        "firstName": "Foo",
+        "id": "5abf67867e"
+    },
+    {
+        "email": "baz@qux.com",
+        "firstName": "Baz",
+        "id": "5abf674e57"
+    }
+]
+module.exports = users;
---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const {response} = require("express");
+const index = express()
+const users = require('./db/users')
+const port = 3001;
+index.use(express.json());
+
+index.get('/users', (req, res) => {
+    res.send(users);
+});
+
+index.post('/add',(req,res) =>{
+    const { email, firstName } = req.body;
+    const id = generateUniqueId();
+
+    const newUser = {
+        email: email,
+        firstName: firstName,
+        id: id
+    };
+
+    users.push(newUser);
+
+    res.json({
+        message: 'User added',
+        success: true,
+        user: newUser
+    });
+
+
+
+});
+
+index.put("/update/:id",(req,res)=>{
+    const id = req.params.id;
+    const { email, firstName } = req.body;
+    const user = users.find(user => user.id === id);
+    if (!user) {
+        return res.status(404).json({
+            message: 'User not found',
+            success: false
+        });
+    }
+
+    user.email = email || user.email;
+    user.firstName = firstName || user.firstName;
+
+    res.json({
+        message: 'User updated successfully',
+        success: true,
+        user: user
+    });
+});
+
+index.get("/user/:id",(req,res)=>{
+    const id = req.params.id;
+
+    const user = users.find(user => user.id === id);
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: 'User not found'
+        });
+    }
+
+    res.json({
+        success: true,
+        user: user
+    });
+
+});
+
+
+function generateUniqueId() {
+    return Math.random().toString(36).substring(2, 9);
+}
+
+index.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
